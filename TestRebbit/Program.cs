e780b353ae955f94f9f56393c5e2fc5d10a2bf1a
oracl <someemail@gmail.com>
2019-11-05 01:02:18 +0200

    First commit

---
+++ TestRebbit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace TestRebbit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+
+            connectionFactory.Port = 5672;
+            connectionFactory.HostName = "127.0.0.1";
+            connectionFactory.UserName = "guest";
+            connectionFactory.Password = "guest";
+            connectionFactory.VirtualHost = "/";
+
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            Console.WriteLine(string.Concat("Connection open: ", connection.IsOpen));
+
+            channel.ExchangeDeclare("my.first.exchange", ExchangeType.Direct, true, false, null);
+            channel.QueueDeclare("my.first.queue", true, false, false, null);
+            channel.QueueBind("my.first.queue", "my.first.exchange", "");
+
+            channel.Close();
+            connection.Close();
+            Console.WriteLine(string.Concat("Channel is closed: ", channel.IsClosed));
+
+            Console.WriteLine("Main done...");
+            Console.ReadKey();
+
+        }
+    }
+}
